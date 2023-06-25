ef28368cb5f3f56ebf715969454ce597d243cb63
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-24 21:28:23 -0700

    added template engine and options

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const sess = {
     }),
   };
   
-  app.use(session(sess));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+  
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
