0b0a57ab4908c12989b4ca903e7625ea5bc6af3f
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-24 23:36:30 -0700

    updated home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,36 @@
 // Imports
+const router = require("express").Router();
+const { BlogPost, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Route to display the homepage
+router.get("/", async (req, res) => {
+    try {
+ 
+      const blogPostData = await BlogPost.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["name"],
+          },
+          {
+            model: Comment,
+            attributes: ["comment_body"],
+          },
+        ],
+      });
+  
+      const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+  
+      res.render("homepage", {
+        blogPosts,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
 
 // Route to display a single blog post
 
@@ -9,7 +39,8 @@
 // Route to display the create page for a new blog post
 
 // Route to display the edit page for an existing blog post
-
+  
 // Route to display the login page
 
 // Export
+module.exports = router;
