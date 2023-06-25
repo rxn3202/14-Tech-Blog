48124a127f019f1192760b26d39c9df28f5e8534
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-24 22:27:16 -0700

    updated post-route.js with delete route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -41,6 +41,26 @@ router.put("/:id", withAuth, async (req, res) => {
 });
 
 // Route to delete an existing blog post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!blogPostData) {
+      res.status(404).json({ message: "No blog post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(blogPostData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
 // Exports
 module.exports = router;
