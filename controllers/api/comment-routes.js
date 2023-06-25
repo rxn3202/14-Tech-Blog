64a2a33166d17368ae3338735aeea36d82500ae9
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-24 21:52:26 -0700

    completed crud operations for comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -19,10 +19,69 @@ router.post('/', async (req, res) => {
 });
 
 // READ all Comments
+router.get('/', async (req, res) => {
+    try {
+      const comments = await Comment.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+          {
+            model: Post,
+            attributes: ['id'],
+          },
+        ],
+      });
+  
+      res.status(200).json(comments);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
 
 // UPDATE Comment
+router.put('/:id', async (req, res) => {
+    try {
+      const [rowsAffected] = await Comment.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (rowsAffected === 0) {
+        res.status(404).json({ message: 'No comment found with that id!' });
+        return;
+      }
+  
+      res.status(200).json({ message: 'Comment updated!' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
 
 // DELETE Comment
-
+router.delete('/:id', async (req, res) => {
+    try {
+      const rowsAffected = await Comment.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (rowsAffected === 0) {
+        res.status(404).json({ message: 'No comment found with that id!' });
+        return;
+      }
+  
+      res.status(200).json({ message: 'Comment deleted!' });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
+  
 // Exports
 module.exports = router;
