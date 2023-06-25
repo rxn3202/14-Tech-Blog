90a6520cb5b9ff87c289fe541115119c53386f58
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-06-25 00:24:38 -0700

    updated post.js for models

---
+++ models/post.js
@@ -0,0 +1,51 @@
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class BlogPost extends Model {}
+
+// BlogPost Table Model
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    underscored: true,
+    modelName: "blogPost",
+    tableName: "blog_posts",
+  }
+);
+
+// Export
+module.exports = BlogPost;
